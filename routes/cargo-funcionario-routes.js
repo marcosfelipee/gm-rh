a835a28e6ccb75b7a7baf999d3de744f0a999d2d
Gabriel de Marco <biel_dmarco@hotmail.com>
2020-03-31 19:23:43 -0300

    add cargo funcionario route relationship

---
+++ routes/cargo-funcionario-routes.js
@@ -0,0 +1,35 @@
+const { Router } = require('express');
+const Cargo = require('../app/models/cargo');
+const Funcionario = require('../app/models/funcionario');
+
+const route = Router();
+
+route.get('/', async (req, res) => {
+  try {
+    const cargos = await Cargo.find();
+
+    // for(let cargo of cargos) {
+    //   cargo.funcionarios = await Funcionario.findById(cargo.funcionario_id);
+    // }
+
+    res.status(200).json({ cargos });
+  } catch(err) {
+    res.status(500).json({
+      erro: "Erro ao buscar cargos"
+    })
+  }
+});
+
+route.get('/:id', async (req, res) => {
+  try {
+    const cargo = await Cargo.findById(id).populate('Funcionários');
+
+    res.status(200).json({ cargo });
+  } catch(err) {
+    res.status(500).json({
+      erro: "Erro ao buscar cargos"
+    })
+  }
+});
+
+module.exports = route;
